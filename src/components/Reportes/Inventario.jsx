a8beaab55b74a91b36a8a61abebdc265ee041b96
Yovani Alvarez <yovani_alvarezz@yahoo.com>
2023-01-03 19:23:25 -0600

    reporte inventario

--- src/components/Reportes/Inventario.jsx
+++ src/components/Reportes/Inventario.jsx
@@ -1,12 +1,10 @@
 import React, { useRef } from 'react'
 import { useReactToPrint } from 'react-to-print'
 import { useEffect, useState } from 'react'
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { PeticionGet } from '../../Servicios/PeticionServicio'
-import numeroAQuetzales from "../../utils/util"
 
 const Inventario = () => {
-    const { idVenta } = useParams()
     const componentRef = useRef();
 
     let creditos = new Array();
@@ -18,67 +16,24 @@ const Inventario = () => {
         documentTitle: 'Inventario',
     })
 
-    const [DataCreditos, setDataCreditos] = useState([{
-        fecha_doc: "",
-        serie_doc: "",
-        numero_doc: "",
+    const [DataArticulos, setDataArticulos] = useState([{
+        categoria:{nombre:""},
+        presentacion:{presentacion: ""},
         nombre: "",
-        persona: {nombre:""},
-        items: [{
-            id:"",
-            cantidad: 0,
-            precio_venta: 0,
-            articulo:{ nombre:""}
-        }]
+        existencia: 0
     }])
 
-    const cargarDataCreditos = async () => {
-        const response = await PeticionGet('Egreso/creditos');
+    const cargarDataArticulos = async () => {
+        const response = await PeticionGet('Articulo/activos');
 
         console.log(response.data.data);
         if(response.data.data.length > 0){
-            armarDataReporte(response.data.data)
+            setDataArticulos(response.data.data)
         }   
-    }
-    
-    const armarDataReporte = (data) => {
-        let articuloEncontrado = false;
-
-        for (let credito of data) {
-            if (credito.pagos.length == 0) {
-                var itemCredito = {
-                    fechaEgreso: credito.fechaegreso,
-                    cliente: credito.persona.nombre,
-                    totalCredito: credito.total_egreso,
-                    descripcion: "",
-                    abono: 0,
-                    saldo: 0
-                }
-                creditos.push(itemCredito);
-            }
-
-            for (let pago of credito.pagos) {
-                var itemCredito = {
-                    fechaEgreso: credito.fechaegreso,
-                    cliente: credito.persona.nombre,
-                    totalCredito: credito.total_egreso,
-                    descripcion: "",
-                    abono: 0,
-                    saldo: 0
-                }
-
-                itemCredito.descripcion = pago.observaciones
-                itemCredito.abono = pago.abono;
-                itemCredito.saldo = pago.saldo
-                creditos.push(itemCredito);
-            }            
-        }
-
-        setDataCreditos(creditos);
     }
 
     useEffect(() => {
-        cargarDataCreditos();
+        cargarDataArticulos();
     }, []);
 
     return (
@@ -94,13 +49,13 @@ const Inventario = () => {
                     
                     <label>Telefono: </label><span class="border-0"> 7758-8956</span><br></br>
                     <label>Email: </label><span class="border-0"> aceiteraemanuel@gmail.com</span><br></br>
-                    {/* <label>Fecha: </label><span class="border-0"> {DataCreditos[0].fecha_doc}</span><br></br>
+                    {/* <label>Fecha: </label><span class="border-0"> {DataArticulos[0].fecha_doc}</span><br></br>
                     <br></br>
 
                     <div>
                         <div>
-                            <span style={{textAlign: 'right'}}><label>Cliente:</label> {DataCreditos[0].persona.nombre.toUpperCase()}</span>
-                            <span style={{marginLeft: '30%', textAlign: 'right'}}><label>Doc:</label> {`${DataCreditos[0].serie_doc.toUpperCase()}-${DataCreditos[0].numero_doc}`}</span>
+                            <span style={{textAlign: 'right'}}><label>Cliente:</label> {DataArticulos[0].persona.nombre.toUpperCase()}</span>
+                            <span style={{marginLeft: '30%', textAlign: 'right'}}><label>Doc:</label> {`${DataArticulos[0].serie_doc.toUpperCase()}-${DataArticulos[0].numero_doc}`}</span>
                             <span style={{marginLeft: '30%', textAlign: 'right'}}><label>Telefono:</label> N/A</span>
                         </div>
                     </div> */}
@@ -108,39 +63,28 @@ const Inventario = () => {
                     <br></br>
                     <table className='w-85 mx-auto table table-striped' >
                         <thead>
-                            <th>Fecha</th>
-                            <th>Cliente</th>
-                            <th>totalCredito</th>
-                            <th>descripcion</th>
-                            <th>abono</th>
-                            <th>saldo</th>
+                            <th>Categoria</th>
+                            <th>Presentacion</th>
+                            <th>Articulo</th>
+                            <th>Existencia</th>
                         </thead>
                         <tbody>
-                            {DataCreditos.map((item, i)=>{
+                            {DataArticulos.map((item, i)=>{
                                 {
-                                    total = (total + item.saldo)
-                                    let datosFecha = item.fechaEgreso.split("-")
-                                    fecha = `${datosFecha[2]}/${datosFecha[1]}/${datosFecha[0]}`
+                                    total = 0
                                 }
                                 return(
                                     <tr>
-                                        <td>{fecha}</td>
-                                        <td>{item.cliente}</td>
-                                        <td>{numeroAQuetzales(item.totalCredito)}</td>
-                                        <td>{item.descripcion}</td>
-                                        <td>{numeroAQuetzales(item.abono)}</td>
-                                        <td>{numeroAQuetzales(item.saldo)}</td>
+                                        <td>{item.categoria.nombre}</td>
+                                        <td>{item.presentacion.presentacion}</td>
+                                        <td>{item.nombre}</td>
+                                        <td>{item.existencia}</td>
                                     </tr>
                                 )      
                             })
                             }
                         </tbody>
                     </table>
-
-                    <div>
-                        <span style={{marginLeft: '65%', textAlign: 'right'}}><label>Total: </label> {numeroAQuetzales(total)}</span>
-                    </div>
-
                 </div>
             </div>
             
