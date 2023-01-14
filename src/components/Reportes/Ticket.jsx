d12e8da90fb3152204d8c9d8a6d9a849a954ad6c
Yovani Alvarez <yovani_alvarezz@yahoo.com>
2023-01-14 11:59:28 -0600

    Correcciones de ticket

--- src/components/Reportes/Ticket.jsx
+++ src/components/Reportes/Ticket.jsx
@@ -2,18 +2,23 @@ import React, { useRef } from 'react'
 import ReactToPrint from 'react-to-print'
 import { useEffect, useState } from 'react'
 import { Link, useParams } from "react-router-dom";
-import { PeticionGet } from '../../Servicios/PeticionServicio'
+import { PeticionGet } from '../../Servicios/PeticionServicio';
+import numeroAQuetzales from "../../utils/util";
 
 const Ticket = () => {
     const { idVenta } = useParams()
     const componentRef = useRef();
     let total = 0;
     let tituloDocumento = "";
+    let fecha = "";
+
     const [DataVentaRealizada, setDataVentaRealizada] = useState([{
+        id: 0,
         fecha_doc: "",
         serie_doc: "",
         numero_doc: "",
         nombre: "",
+        usuario: {nombre: ""},
         persona: { nombre: "" },
         items: [{
             id: "",
@@ -27,9 +32,23 @@ const Ticket = () => {
         const response = await PeticionGet(`Egreso/id/${idVenta}`);
 
         if (response.data.data.length > 0) {
-            setDataVentaRealizada(response.data.data);
-            tituloDocumento = `Tiket_${DataVentaRealizada.numero_doc}`;
+            console.log(response.data.data)
+            armarData(response.data.data);
         }
+    }
+
+    const armarData = (data) => {
+        tituloDocumento = `Tiket_${data[0].id}`;
+
+        const datosFecha = data[0].fecha_doc.split("-")
+
+        if(datosFecha.length == 3){
+            fecha = `${datosFecha[2]}/${datosFecha[1]}/${datosFecha[0]}`
+            console.log(fecha)
+            data[0].fecha_doc = fecha;
+        }
+
+        setDataVentaRealizada(data);
     }
 
     useEffect(() => {
@@ -45,28 +64,31 @@ const Ticket = () => {
                 pageStyle='@page { size: 80mm 258mm; margin: 0mm; } @media print { body { -webkit-print-color-adjust: exact; padding: 80px !important; } }'
             />
             <Link className="btn btn-sm btn-outline-danger px-3 " to="/ventasRealizadas"> cancelar</Link>
-            <hr></hr>
+            <hr style={{ width: '50%', marginLeft: '25%' }}></hr>
             <div style={{ width: '95%', marginLeft: '35%' }}>
                 <div ref={componentRef} >
                     <style type="text/css" media="print">{"\
-                    @page {\ size: 80mm 258mm;\ }\
-                "}
+                        @page {\ size: 80mm 258mm;\ }\
+                    "}
                     </style>
                     <div style={{ width: '35%', textAlign: 'center' }}>
                         <label><h2>Aceitera Emanuel</h2> </label><br></br>
-                        <label>Nit: </label><b class="border-0"> 7455225-5</b><br></br>
-                        <label>Telefono: </label><h5 class="border-0"> 7758-8956</h5><br></br>
-                        <label>Email: </label><h5 class="border-0"> aceiteraemanuel@gmail.com</h5><br></br>
-                        <p>---------------------------------------------</p>
+                        <label>Nit: </label><span class="border-0"> 7455225-5</span><br></br>
+                        <label>Telefono: </label><span class="border-0"> 7758-8956</span><br></br>
+                        <label>Email: </label><span class="border-0"> aceiteraemanuel@gmail.com</span><br></br>
+                        <label>Vendedor: </label><span class="border-0"> {DataVentaRealizada[0].usuario.nombre}</span><br></br>
+                        <hr></hr>
+
+                        <label>No. Ticket: </label><span class="border-0"> {DataVentaRealizada[0].id}</span><br></br>
                         <label>Fecha: </label><span class="border-0"> {DataVentaRealizada[0].fecha_doc}</span><br></br>
                         <span><label>Doc:</label> {`${DataVentaRealizada[0].serie_doc.toUpperCase()}-${DataVentaRealizada[0].numero_doc}`}</span><br></br>
                         <span><label>Cliente:</label> {DataVentaRealizada[0].persona.nombre.toUpperCase()}</span><br></br>
                         <span><label>Telefono:</label> N/A</span>
-                        <p>---------------------------------------------</p>
+                        <hr></hr>
                         <table className='w-85 mx-auto table table-borderless table-sm' >
                             <thead>
-                                <th>Nombre</th>
-                                <th>Cantidad</th>
+                                {/* <th>Nombre</th> */}
+                                <th>Cant.</th>
                                 <th>Precio</th>
                                 <th>Subtotal</th>
                             </thead>
@@ -78,22 +100,25 @@ const Ticket = () => {
                                     return (
                                         
                                         <tr key={item.id} >
-                                            <td>{item.articulo.nombre}</td>
-                                            <td>{item.cantidad}</td>
-                                            <td>{item.precio_venta}</td>
-                                            <td>Q {item.precio_venta * item.cantidad}</td>
+                                            {/* <td>{item.articulo.nombre}</td> */}
+                                            <td style={{ verticalAlign: 'middle' }}>{item.cantidad}</td>
+                                            <td>
+                                                {item.articulo.nombre}
+                                                <p>{numeroAQuetzales(item.precio_venta)}</p>
+                                            </td>
+                                            <td style={{ verticalAlign: 'middle' }}>{numeroAQuetzales(item.precio_venta * item.cantidad)}</td>
                                         </tr>
                                     )
                                 })
                                 }
                             </tbody>
                         </table>
-                        <p>---------------------------------------------</p>
+                        <hr></hr>
                         <div style={{ textAlign: 'right' }}>
-                            <span style={{ textAlign: 'right' }}><label>Total a pagar: </label> Q {total}</span><br /><br />
+                            <span style={{ textAlign: 'right' }}><label>Total a pagar: </label> {numeroAQuetzales(total)}</span><br /><br />
                         </div>
                         <h5 className='d-flex justify-content-center'>Gracias por su compra</h5>
-                        <h6 className='d-flex justify-content-center'>*** NO SE ACEPTAN CAMBIOS NI DEVOLUCIONES.</h6>
+                        <span className='d-flex justify-content-center'>*** NO SE ACEPTAN CAMBIOS NI DEVOLUCIONES.***</span>
                     </div>
                 </div>
             </div>
