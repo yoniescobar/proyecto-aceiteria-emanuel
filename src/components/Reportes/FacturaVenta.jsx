3bd77e44b7572dcb7b7fe9e22cb27fa30a7ff989
Yovani Alvarez <yovani_alvarezz@yahoo.com>
2023-01-15 13:46:18 -0600

    Correccion reporte factura

--- src/components/Reportes/FacturaVenta.jsx
+++ src/components/Reportes/FacturaVenta.jsx
@@ -21,34 +21,34 @@ const FacturaVenta = () => {
         serie_doc: "",
         numero_doc: "",
         nombre: "",
-        persona: {nombre:""},
+        persona: { nombre: "" },
         items: [{
-            id:"",
+            id: "",
             cantidad: 0,
             precio_venta: 0,
-            articulo:{ nombre:""}
+            articulo: { nombre: "" }
         }]
     }])
 
     const cargarDataVentaRealizada = async () => {
         const response = await PeticionGet(`Egreso/id/${idVenta}`);
 
-        if(response.data.data.length > 0){
+        if (response.data.data.length > 0) {
             armarData(response.data.data)
-        }   
+        }
     }
 
     const armarData = (data) => {
         const datosFecha = data[0].fecha_doc.split("-")
 
-        if(datosFecha.length == 3){
+        if (datosFecha.length == 3) {
             fecha = `${datosFecha[2]}/${datosFecha[1]}/${datosFecha[0]}`
             data[0].fecha_doc = fecha;
         }
 
         setDataVentaRealizada(data);
     }
-    
+
     useEffect(() => {
         cargarDataVentaRealizada();
     }, []);
@@ -62,21 +62,21 @@ const FacturaVenta = () => {
                 </h1>
                 <hr></hr>
 
-                <div ref={componentRef} style={{margin: '10%', margin: '10%', marginTop: '5%', marginRight: '10%', marginBottom: '10%', marginLeft: '10%'}}>
-                    <h1 style={{textAlign: 'center'}}>Aceitera Emanuel</h1>
+                <div ref={componentRef} style={{ margin: '10%', margin: '10%', marginTop: '5%', marginRight: '10%', marginBottom: '10%', marginLeft: '10%' }}>
+                    <h1 style={{ textAlign: 'center' }}>Aceitera Emanuel</h1>
                     <br></br>
                     <label>Nit: </label><span class="border-0"> 7455225-5</span><br></br>
-                    
+
                     <label>Telefono: </label><span class="border-0"> 7758-8956</span><br></br>
                     <label>Email: </label><span class="border-0"> aceiteraemanuel@gmail.com</span><br></br>
                     <label>Fecha: </label><span class="border-0"> {DataVentaRealizada[0].fecha_doc}</span><br></br>
                     <br></br>
 
                     <div>
-                        <div style={{textAlign: 'center'}}>
-                            <span style={{textAlign: 'right'}}><label>Cliente:</label> {DataVentaRealizada[0].persona.nombre.toUpperCase()}</span>
-                            <span style={{marginLeft: '10%', textAlign: 'center'}}><label>Doc:</label> {`${DataVentaRealizada[0].serie_doc.toUpperCase()}-${DataVentaRealizada[0].numero_doc}`}</span>
-                            <span style={{marginLeft: '10%', textAlign: 'right'}}><label>Telefono:</label> N/A</span>
+                        <div style={{ textAlign: 'center' }}>
+                            <span style={{ textAlign: 'right' }}><label>Cliente:</label> {DataVentaRealizada[0].persona.nombre.toUpperCase()}</span>
+                            <span style={{ marginLeft: '10%', textAlign: 'center' }}><label>Doc:</label> {`${DataVentaRealizada[0].serie_doc.toUpperCase()}-${DataVentaRealizada[0].numero_doc}`}</span>
+                            <span style={{ marginLeft: '10%', textAlign: 'right' }}><label>Telefono:</label> N/A</span>
                         </div>
                     </div>
 
@@ -89,24 +89,24 @@ const FacturaVenta = () => {
                             <th>Subtotal</th>
                         </thead>
                         <tbody>
-                            {DataVentaRealizada[0].items.map((item, i)=>{
+                            {DataVentaRealizada[0].items.map((item, i) => {
                                 {
                                     total = (total + (item.precio_venta * item.cantidad))
                                 }
-                                return(
+                                return (
                                     <tr key={item.id}>
                                         <td>{item.articulo.nombre}</td>
                                         <td>{item.cantidad}</td>
                                         <td>{item.precio_venta}</td>
                                         <td>{numeroAQuetzales(item.precio_venta * item.cantidad)}</td>
                                     </tr>
-                                )      
+                                )
                             })
                             }
                         </tbody>
                     </table>
-                    <div style={{textAlign: 'right'}}>
-                            <span><label>Total a pagar: </label> {numeroAQuetzales(total)}</span>
+                    <div style={{ textAlign: 'right' }}>
+                        <span><label>Total a pagar: </label> {numeroAQuetzales(total)}</span>
                     </div>
                 </div>
             </div>
