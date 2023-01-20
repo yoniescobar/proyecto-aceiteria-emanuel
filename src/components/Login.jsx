6a1fef4988c55cdbbff7d831c2263779d0c82424
Yovani Alvarez <yovani_alvarezz@yahoo.com>
2023-01-19 22:49:20 -0600

    correccion de id en localstorage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,119 +8,114 @@ import { alertMensaje } from '../utils/alert';
 const baseUrl = process.env.REACT_APP_BASE_URL
 
 const Login = () => {
-    const [error, setError] = useState(null);
-    const [form, SetForm] = useState({
-      username: "",
-      password: "",
+  const [error, setError] = useState(null);
+  const [form, SetForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setError(null);
+    const { name, value } = e.target;
+
+    SetForm({
+      ...form,
+      [name]: value,
     });
+  };
 
-    const handleChange = (e) => {
-        setError(null);
-        const { name, value } = e.target;
-    
-        SetForm({
-          ...form,
-          [name]: value,
-        });
-    };
-    
-    const onSubmit = async (event) => {
-      event.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-      // const response = await PeticionGet('Usuario/all');
-      const response = await PeticionGet(`Usuario/usuario/${form.username}/password/${form.password}`);
-      
+    // const response = await PeticionGet('Usuario/all');
+    const response = await PeticionGet(`Usuario/usuario/${form.username}/password/${form.password}`);
 
-      if (response && response.data.data.length == 1) {
-        validarUsuario(response);
-      }
+    if (response && response.data.data.length == 1) {
+      validarUsuario(response.data.data[0]);
+    } else {
+      alertMensaje('Datos no encontrados, verifique sus credenciales.', 'warning')
     }
-    
-    const validarUsuario = (response) => {
-      const usuario = response.data.data;
+  }
 
-      if (usuario) {
-          setUserSession(usuario.usuario, usuario.usuario, usuario.id);
-          window.location.reload();
-      } else {
-        alertMensaje('Datos no encontrados, verifique sus credenciales.', 'warning')
-      }
-    }
+  const validarUsuario = (usuario) => {
+    setUserSession(usuario.usuario, usuario.usuario, usuario.id);
+    window.location.reload();
+  }
 
   return (
     <div>
-    <Container>
-      <Row className="vh-100 d-flex justify-content-center align-items-center">
-        <Col md={8} lg={5} xs={12}>
-          <Card className="shadow">
-            <Card.Body>
-              <div className="mb-3 mt-md-4">
-                <div className="mb-12" style={{ textAlign: "center" }}>
-                  {/* <img src={require("../img/logo-1.png")} /> */}
-                </div>
-                <p className=" mb-5"></p>
-                <div className="mb-3">
-                  <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label className="text-center">
-                        Usuario:
-                      </Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Ingrese su usuario"
-                        name="username"
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                    >
-                      <Form.Label>Contraseña:</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Ingrese su Contraseña"
-                        name="password"
-                        onChange={handleChange}
-                      />
-                    </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicCheckbox"
-                    ></Form.Group>
-                    {error && (
-                      <div
-                        className="alert alert-danger alert-dismissible fade show"
-                        role="alert"
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={5} xs={12}>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <div className="mb-12" style={{ textAlign: "center" }}>
+                    {/* <img src={require("../img/logo-1.png")} /> */}
+                  </div>
+                  <p className=" mb-5"></p>
+                  <div className="mb-3">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Usuario:
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Ingrese su usuario"
+                          name="username"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
                       >
-                        <strong>Alerta! &nbsp;</strong>
-                        {error}
+                        <Form.Label>Contraseña:</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Ingrese su Contraseña"
+                          name="password"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicCheckbox"
+                      ></Form.Group>
+                      {error && (
+                        <div
+                          className="alert alert-danger alert-dismissible fade show"
+                          role="alert"
+                        >
+                          <strong>Alerta! &nbsp;</strong>
+                          {error}
+                        </div>
+                      )}
+                      <div className="d-grid">
+                        <Button
+                          onClick={onSubmit}
+                          style={{
+                            backgroundColor: "#213158",
+                          }}
+                        >
+                          Iniciar Sesión
+                        </Button>
                       </div>
-                    )}
-                    <div className="d-grid">
-                      <Button
-                        onClick={onSubmit}
-                        style={{
-                          backgroundColor: "#213158",
-                        }}
-                      >
-                        Iniciar Sesión
-                      </Button>
-                    </div>
-                  </Form>
+                    </Form>
+                  </div>
                 </div>
-              </div>
-            </Card.Body>
-          </Card>
-          <div className="footer-copyright text-center py-3">
-            {" "}
-            <Form.Label style={{ textDecoration: "white" }}>
-            </Form.Label>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+              </Card.Body>
+            </Card>
+            <div className="footer-copyright text-center py-3">
+              {" "}
+              <Form.Label style={{ textDecoration: "white" }}>
+              </Form.Label>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
