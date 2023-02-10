32dd5aa4cf2ca1fb134d1d1fba2c6401dc1d1ea4
Miguel Angel Menchú Xoyón <michole77@hotmail.com>
2023-02-09 21:34:54 -0600

    cambios en servicio de compras

--- src/components/compras/servicios.js
+++ src/components/compras/servicios.js
@@ -1,1 +1,11 @@
 const baseUrl = process.env.REACT_APP_BASE_URL;
+
+
+export const getItemByCode = (code) => {
+    return fetch(baseUrl+'/Articulo/codigo/'+code, { // add return here
+                  method:'Get',
+              })
+              .then(data=> {
+               return   data.json()
+              })
+}
